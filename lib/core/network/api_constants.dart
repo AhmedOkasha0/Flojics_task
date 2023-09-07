c4b7ca0b0fca0f5b6d27142a64cbcb55f4bce592
ahmed okasha <ao1922302@gmail.com>
2023-09-08 02:42:15 +0300

    add repository data class and movies data source , add network api constants and error class

---
+++ lib/core/network/api_constants.dart
@@ -0,0 +1,6 @@
+class ApiConstants {
+  static const String moviesEndPoint =
+      "https://mcuapi.herokuapp.com/api/v1/movies";
+  static const String tvShowsEndPoint =
+      "https://mcuapi.herokuapp.com/api/v1/tvshows";
+}
