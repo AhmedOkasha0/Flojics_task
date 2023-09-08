f0b00b4166a48021c7bd1d6157a5c45551defa5d
ahmed okasha <ao1922302@gmail.com>
2023-09-08 17:07:24 +0300

    add firebase package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flojics_task/config/routes/app_router.dart';
 import 'package:flojics_task/core/services/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   ServiceLocator().init();
   runApp(const MyApp());
 }
